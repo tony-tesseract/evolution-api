2908fe32d037baa38806ec7b263801cc7335fbd1
tony-tesseract <tony@tesseract-ai.net>
2025-05-21 20:49:06 -0300

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,22 @@
 import { cpSync } from 'node:fs';
-
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src'],
+  entry: ['src/main.ts'], // define ponto de entrada explícito
   outDir: 'dist',
+  format: ['cjs'],        // apenas 'cjs' para compatibilidade Node.js
   splitting: false,
   sourcemap: true,
   clean: true,
   minify: true,
-  format: ['cjs', 'esm'],
+  target: 'es2020',       // recomendável para Node.js 16+
+  dts: false,             // não precisa de arquivos .d.ts no dist
+
   onSuccess: async () => {
     cpSync('src/utils/translations', 'dist/translations', { recursive: true });
   },
+
   loader: {
-    '.json': 'file',
+    '.json': 'file',      // permite importar JSON como recurso
   },
 });
